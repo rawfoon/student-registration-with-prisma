298d5482c603706da16dda68f4586cc9978c1a87
rawfoon <mgrawfoon@gmail.com>
2023-12-24 01:42:25 +0600

    student update finished

---
+++ src/app/api/updateStudent/route.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client"
+import { NextResponse } from "next/server"
+
+export async function POST(req,res){
+    
+    try{
+
+        let {searchParams} = new URL(req.url)
+        const id = parseInt(searchParams.get('id')) 
+        // console.log(parseInt(searchParams.get('id')));
+
+        const reqBody = await req.json()
+
+        const prisma = new PrismaClient()
+        let result = await prisma.Users.update({
+            where:{id:id},
+            data: reqBody
+
+           
+
+        })
+
+        return NextResponse.json({status:"success", data: result})
+    }
+    catch (e){
+        return NextResponse.json({status:"fail", data: e})
+    }
+}
