de9d440f92061e1c4fb50ce9e841cd29a46f92b3
rawfoon <mgrawfoon@gmail.com>
2023-12-24 02:11:21 +0600

    get all students data  completed

---
+++ src/app/api/getAllStudents/route.js
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client"
+import { NextResponse } from "next/server"
+
+
+export async function POST(req,res){
+
+    try{
+     
+        const prisma = new PrismaClient()
+        let result = await prisma.Users.findMany()
+
+
+            return NextResponse.json({status:"success", data: result})
+    }
+
+    catch (e) {
+        return NextResponse.json({status:"fail", data:e})
+    }
+}
